b80862e7386f45e968907bf723fe6542858ac60b
kevin <nivekbryan09.projects@gmail.com>
2020-05-16 15:53:36 +0800

    Updates

---
+++ precache-manifest.1824d81458cc91ebeb0932b106318c37.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0352215fff61aa163da0da1a1d440201",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f50452c4d89025c9ebc7",
+    "url": "/portfolio/static/css/main.a371e9b9.chunk.css"
+  },
+  {
+    "revision": "03ed60b72c105139751f",
+    "url": "/portfolio/static/js/2.93c68544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.93c68544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f50452c4d89025c9ebc7",
+    "url": "/portfolio/static/js/main.16658f20.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "268e89298b9317286fce66edec9efcea",
+    "url": "/portfolio/static/media/Segoe UI Bold Italic.268e8929.woff"
+  },
+  {
+    "revision": "52382539737f4e9913e4bf6b9966bee3",
+    "url": "/portfolio/static/media/Segoe UI Bold.52382539.woff"
+  },
+  {
+    "revision": "347c3ac06f20b6a969fc55e89b37c9c4",
+    "url": "/portfolio/static/media/Segoe UI Italic.347c3ac0.woff"
+  },
+  {
+    "revision": "9a2931180d6b1dc7b33052657eef554b",
+    "url": "/portfolio/static/media/Segoe UI.9a293118.woff"
+  },
+  {
+    "revision": "c794496a60c0f66eb6bc6c129e45acfb",
+    "url": "/portfolio/static/media/b&w.c794496a.png"
+  },
+  {
+    "revision": "8c0e1bb9a4742149ed6133a6032b8468",
+    "url": "/portfolio/static/media/black and white.8c0e1bb9.png"
+  },
+  {
+    "revision": "b54235970cdf129fe53147187a813131",
+    "url": "/portfolio/static/media/cities.b5423597.png"
+  },
+  {
+    "revision": "a87d09629390683fad04c0ad90e0a01e",
+    "url": "/portfolio/static/media/faceoff.a87d0962.png"
+  },
+  {
+    "revision": "c5d2369e922beb7466b03e1cc4c3ff67",
+    "url": "/portfolio/static/media/faster.c5d2369e.png"
+  },
+  {
+    "revision": "a160cfff448db74cf2cb6e4746031845",
+    "url": "/portfolio/static/media/love or hate.a160cfff.png"
+  },
+  {
+    "revision": "57cfb137a6b2806ece9ac9d047df381c",
+    "url": "/portfolio/static/media/love trap.57cfb137.png"
+  },
+  {
+    "revision": "248f647e2637571b3a694709c3913b9b",
+    "url": "/portfolio/static/media/mophoria.248f647e.png"
+  },
+  {
+    "revision": "73f0516195d6d737639c35c5abf30efb",
+    "url": "/portfolio/static/media/puffypuff.73f05161.png"
+  },
+  {
+    "revision": "2afae792c5e4c1e0bfe63058e0c6160f",
+    "url": "/portfolio/static/media/smartserver.2afae792.png"
+  },
+  {
+    "revision": "22149d4f3280ddbe0663e17f64c4b626",
+    "url": "/portfolio/static/media/traveler_europe.22149d4f.png"
+  },
+  {
+    "revision": "dd42336bc85e58ff6a095627c9ba60ae",
+    "url": "/portfolio/static/media/traveler_philippines.dd42336b.png"
+  }
+]);
